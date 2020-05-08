a38fc4416c0583df14dad08b1449eb4a87116d11
Talia Harrison-Marcassa <hm.talia@gmail.com>
2020-05-08 15:49:52 -0400

    Updates

---
+++ precache-manifest.4f476c3c7e72c506b9186e5da14108da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e446f3e7490ad14ef90afeba36a4dd7",
+    "url": "/testingstuff/index.html"
+  },
+  {
+    "revision": "820f2c0365031480d868",
+    "url": "/testingstuff/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e4c9fd9502ba07ae314d",
+    "url": "/testingstuff/static/js/2.424e5fbe.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/testingstuff/static/js/2.424e5fbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820f2c0365031480d868",
+    "url": "/testingstuff/static/js/main.4b95ff5e.chunk.js"
+  },
+  {
+    "revision": "44a21180cff76c343760",
+    "url": "/testingstuff/static/js/runtime-main.11913c55.js"
+  }
+]);
